79f5599a488a6c7c649f9702425e17416bac8830
Zacharia Mwihechi <zachariamwihechi@gmail.com>
2022-04-27 18:07:26 +0300

    basic application

---
+++ src/main/java/com/tansoften/msd/MsdApplication.java
@@ -0,0 +1,16 @@
+package com.tansoften.msd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsdApplication.class, args);
+		System.out.println("Test ");
+		System.out.println("Test ");
+		System.out.println("Test ");
+	}
+
+}
