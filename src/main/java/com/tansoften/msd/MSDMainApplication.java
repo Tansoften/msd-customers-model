8c1916e3502290b3e6d8bf3c91e0744dfdc42574
Yogona <yonagodwin@gmail.com>
2022-04-28 00:23:52 +0300

    changes

--- src/main/java/com/tansoften/msd/MSDMainApplication.java
+++ src/main/java/com/tansoften/msd/MSDMainApplication.java
@@ -1,9 +1,17 @@
 package com.tansoften.msd;
 
+import com.tansoften.msd.data.Customer;
 import org.springframework.boot.SpringApplication;
 
-public class MSDMainApplication {
-    public static void main(String[] args) {
+import java.util.ArrayList;
 
+public class MSDMainApplication {
+    private ArrayList<Customer> root = new ArrayList<>();
+
+    public static void main(String[] args) {
+        for(datum:data){
+            Customer customer = new Customer();
+            customer.setId(datum.get("id"));
+        }
     }
 }
