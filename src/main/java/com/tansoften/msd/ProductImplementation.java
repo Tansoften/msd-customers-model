27e4270803d3e6a18426fb84cf267338135aa47c
Zacharia Mwihechi <zachariamwihechi@gmail.com>
2022-04-28 00:34:04 +0300

    base application

--- src/main/java/com/tansoften/msd/ProductImplementation.java
+++ src/main/java/com/tansoften/msd/ProductImplementation.java
@@ -1,5 +1,34 @@
 package com.tansoften.msd;
 
-public class CustomerImplementation {
-    
+import com.tansoften.msd.data.Customer;
+import com.tansoften.msd.data.Product;
+
+import java.util.ArrayList;
+
+public class ProductImplementation {
+
+    int productId;
+    ArrayList<Product> productsList = new ArrayList<>();
+
+    ProductImplementation(int productId){
+        this.productId = productId;
+    }
+
+
+
+    private void setProduct(int productId){
+        if (productsList.isEmpty()){
+            Product product = new Product(this.productId);
+            productsList.add(product);
+        }else {
+            productsList.forEach(itemProduct -> {
+                if (itemProduct.getId() != productId){
+                    Product product = new Product(this.productId);
+                    productsList.add(product);
+                }
+            });
+        }
+    }
+
+
 }
--- src/main/java/com/tansoften/msd/data/Customer.java
+++ src/main/java/com/tansoften/msd/data/Customer.java
@@ -1,5 +1,7 @@
 package com.tansoften.msd.data;
 
+import java.util.ArrayList;
+
 public class Customer {
    private int id;
 
