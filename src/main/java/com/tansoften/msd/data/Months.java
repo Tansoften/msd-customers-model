16c743f29bf7cab9daa6d4de006af2df13e04cfc
Yogona <yonagodwin@gmail.com>
2022-04-29 18:24:52 +0300

    Geometric mean is the best

--- src/main/java/com/tansoften/msd/data/Months.java
+++ src/main/java/com/tansoften/msd/data/Months.java
@@ -4,7 +4,6 @@ import com.tansoften.msd.ModelTesting;
 import com.tansoften.msd.STATUS;
 
 import java.util.ArrayList;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class Months {
   private  int id;
@@ -15,19 +14,13 @@ public class Months {
     }
 
     public int determineConsumption(){
-        AtomicInteger total= new AtomicInteger();
-        consumptions.stream().map((consumption -> {
-            total.addAndGet(consumption.getQuantity());
-            return null;
-        })).toList();
-
         try{
-           //return ModelTesting.getSumOfThreeNo(consumptions);
-           //return ModelTesting.getRandomGhost(consumptions);
-            return ModelTesting.calculateMedianConsumption(consumptions);
+            //return ModelTesting.getSumOfThreeNo(consumptions);
+            //return ModelTesting.getRandomGhost(consumptions);
+            //return ModelTesting.calculateMedianConsumption(consumptions);
             //return ModelTesting.getLatestConsumption(consumptions);
-            //return ModelTesting.calculateGeometricMean(consumptions);
-          // return ModelTesting.findMean(total, consumptions.size());
+            return ModelTesting.calculateGeometricMean(consumptions);
+            //return ModelTesting.calculateArithmeticMean(consumptions);
         }catch (Exception exc){
             return STATUS.ZERO_DIVIDE.ordinal();
         }
